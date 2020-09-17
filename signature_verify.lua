f379184723516f1c35ace115a042a5414638e9df
bomee <shiaupo@qq.com>
2020-09-17 19:36:59 +0800

    Update signature_verify.lua

--- signature_verify.lua
+++ signature_verify.lua
@@ -1,17 +1,23 @@
 local secret_table = {
-    v1 = "secret_v1",
-    v2 = "secret_v2"
+    v1 = "R2$ty@keWUwWj7fVDj@&VUgsCkAGF6yy",
+    v2 = "&E00bAS%9QYH@yHI^E8s*@mo6WvxZ7Zk",
+    v3 = "o@ZR&x7rE5T%&bJ$iZA51Db257BDfht1",
+    v4 = "TKjO$04u7gQw$D%PrpYz4LeFWe&LUx&s",
+    v5 = "OXO11FdNJ0Pgo1%8mOBtGm3DN!*ipdo0"
 }
 
-local STATUS_PRECONDITION_REQUIRED = 428
+local STATUS_PRECONDITION_FAILED = 412
 
+local function verify_fail(message)
+    ngx.log(ngx.WARN, message)
+    -- ngx.status = STATUS_PRECONDITION_FAILED
+    -- ngx.say(message)
+    -- ngx.exit(STATUS_PRECONDITION_FAILED)
+    return
+end
 
 if ngx.var.query_string == nil then
-    ngx.log(ngx.WARN, "Required parameter missing.")
-    ngx.status = STATUS_PRECONDITION_REQUIRED
-    ngx.say("Required parameter missing.")
-    ngx.exit(STATUS_PRECONDITION_REQUIRED)
-    return
+    return verify_fail("Required parameter missing.")
 end
 
 local function safe_str(value)
@@ -22,15 +28,15 @@ local function safe_str(value)
     end
 end
 
--- parse query_string
+-- Parse query_string to table for sort
 local query_table = {}
-local ts = nil
+local sign_ts = nil
 local sign = nil
 local sign_v = nil
 local qt_idx = 1
 for str in string.gmatch(ngx.var.query_string, "([^&]+)&?") do 
-    if string.find(str, "ts=") then
-        ts = string.gsub(str, "ts=", "", 1)
+    if string.find(str, "sign_ts=") then
+        sign_ts = string.gsub(str, "sign_ts=", "", 1)
     elseif string.find(str, "sign=") then
         sign = string.gsub(str, "sign=", "", 1)
         goto next
@@ -42,31 +48,19 @@ for str in string.gmatch(ngx.var.query_string, "([^&]+)&?") do
     ::next::
 end
 
-if sign == nil or ts == nil or sign_v == nil then
-    local out = "Required parameter missing: sign=" .. safe_str(sign_v) .. 
-                ", ts=" .. safe_str(ts) .. 
-                ", sign_v=" .. safe_str(sign_v)
-    ngx.log(ngx.WARN, out)
-    ngx.status = STATUS_PRECONDITION_REQUIRED
-    ngx.say(out)
-    ngx.exit(STATUS_PRECONDITION_REQUIRED)
-    return
+if sign == nil or sign_ts == nil or sign_v == nil then
+    return verify_fail(out) 
 end
 
 -- Timestamp difference check
-if (os.time() - tonumber(ts)) > 5 * 60 then
-    ngx.log(ngx.WARN, "The time difference should not be more than 5 minutes")
-    ngx.status = STATUS_PRECONDITION_REQUIRED
-    ngx.say("The time difference should not be more than 5 minutes")
-    ngx.exit(STATUS_PRECONDITION_REQUIRED)
+if (os.time() - tonumber(sign_ts)) > 5 * 60 then
+    return verify_fail("The time difference should not be more than 5 minutes")
 end
 
 table.sort(query_table)
 
--- md5 body
-if ngx.var.http_content_type ~= nil and (string.find(ngx.var.http_content_type, 'json') or
-    string.find(ngx.var.http_content_type, 'www-form-urlencoded'))
-then
+-- Md5 body
+if ngx.var.http_content_type ~= nil and (string.find(ngx.var.http_content_type, 'json') or string.find(ngx.var.http_content_type, 'www-form-urlencoded')) then
     ngx.req.read_body()
     local req_body = ngx.req.get_body_data()
     local body_md5 = ngx.md5(req_body)
@@ -75,11 +69,7 @@ end
 local secret = secret_table[sign_v]
 
 if secret == nil then
-    ngx.log(ngx.WARN, "Unsupported signature version: " .. sign_v);
-    ngx.status = STATUS_PRECONDITION_REQUIRED
-    ngx.say("Unsupported signature version: " .. sign_v)
-    ngx.exit(STATUS_PRECONDITION_REQUIRED)
-    return
+    return verify_fail("Unsupported signature version: " .. sign_v)
 end
 
 -- Signature algorithm：base64(hmac_sha1(secrect, uri + sort(query_string) + md5(body)))
@@ -96,8 +86,5 @@ end
 local sign_by_server = ngx.encode_base64(ngx.hmac_sha1(secret, sign_str))
 if sign_by_server ~= sign then
     ngx.log(ngx.WARN, "Signature verify failure: sign_str=" .. sign_str.. ", client_sign=" .. sign .. ", server_sign=" .. sign_by_server);
-    ngx.status = STATUS_PRECONDITION_REQUIRED
-    ngx.say("Illegal signature: "..sign_str)
-    ngx.exit(STATUS_PRECONDITION_REQUIRED)
-    return
+    return verify_fail("Illegal signature")
 end
